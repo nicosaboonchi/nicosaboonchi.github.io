4a05b4f75ace49ade352416ae72438b141d0f654
Nico Saboonchi <nico.saboonchi@gmail.com>
2025-02-07 14:03:40 -0800

    changed yes button image

--- scripts/script.js
+++ scripts/script.js
@@ -2,13 +2,21 @@ const header = document.querySelector("h1");
 const yesBtn = document.querySelector(".yes");
 const noBtn = document.querySelector(".no");
 const img = document.querySelector("img");
+const images = ["blowing-kiss.gif", "i-love-you.gif", "bear-love.gif"];
 
 yesBtn.addEventListener("click", () => {
-  img.src = "images/blowing-kiss.gif";
+  img.src = "images/bear-love.gif";
   img.alt = "a cartoon heart blowing a kiss";
 
   yesBtn.remove();
   noBtn.remove();
 
   header.innerHTML = "YAY! ❤️<br>Happy Valentines Day!";
+});
+
+noBtn.addEventListener("click", () => {
+  img.src = "images/angry-cat.gif";
+  img.alt = "grumpy looking cat";
+
+  header.textContent = "Did you really just hit no...?";
 });
